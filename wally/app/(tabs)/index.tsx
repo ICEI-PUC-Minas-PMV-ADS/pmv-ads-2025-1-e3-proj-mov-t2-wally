07eda9afc2904a10d1b721fb1f6cae325f8de40b
Ariane Moura <arianemouraa6@gmail.com>
2025-05-07 12:59:07 -0300

    add header dashboard principal

--- wally/app/(tabs)/index.tsx
+++ wally/app/(tabs)/index.tsx
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 
 export default function Wallet(){
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+       <View style={styles.header}>
+       <Image
+          source={require('../../assets/images/logo.png')}
+          style={styles.logo}/>
+       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F2F2',
+  },
+  header: {
+    backgroundColor: '#9ACBD0',
+    height: 100,
+    paddingTop: 60,
+  },
+  logo: {
+    width: 96,
+    height: 96,
+    marginTop: 60,
+    alignSelf: 'center',
+  }
+})
